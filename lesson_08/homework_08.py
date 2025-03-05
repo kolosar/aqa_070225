83ce7ac3fb5a91fc13cc81e6119cebe246cbbb45
kolosar <verakolosar@gmail.com>
2025-03-05 11:00:53 +0200

    homework_08 is done

--- lesson_08/homework_08.py
+++ lesson_08/homework_08.py
@@ -14,13 +14,22 @@ sum_numbers_in_list("21")  # ValueError
 ```
 """
 
-
 def sum_numbers_in_list(string_list: list):
     """Повертає список сум чисел зі списку строк,
     які складаються з чисел, розділених комою."""
+    result = [] 
+    
+    for line in string_list:
+        try:
+            # Робимо спліт строки, розділеною комою, конвертуємо в int
+            numbers = list(map(int, line.split(',')))
+            # Додаємо сумми елементів в список
+            result.append(sum(numbers))
+        except (ValueError, AttributeError, AssertionError):
+            result.append(f"Error: Invalid data in '{line}'")
 
     
-    return
+    return result
 
 
 if __name__ == "__main__":
@@ -29,10 +38,10 @@ if __name__ == "__main__":
 
     output = sum_numbers_in_list(["1,2,3", "4/0,6", "asas7,8,9"])
     print(output)
-    """
+    
     sum_numbers_in_list(["1,2,3", "4,0,6"])  # [6, 10]
     sum_numbers_in_list(["1,2,3", "asas7,8,9", "4,0,6"])  # [6, "Не можу це зробити!", 10]
     sum_numbers_in_list(["1,2,3,4", 7])  # [10, "Не можу це зробити! AttributeError"]
     sum_numbers_in_list([])  # ValueError
     sum_numbers_in_list("21")  # ValueError
-    """
+    
