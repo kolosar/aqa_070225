7130c7f66c6ff0f8d42f7bf95557ab4f8460720b
kolosar <verakolosar@gmail.com>
2025-03-12 12:02:18 +0100

    Homework_09

--- lesson_09/homework_09.py
+++ lesson_09/homework_09.py
@@ -4,3 +4,20 @@
 який дозволяє змінювати середній бал студента. Виведіть інформацію про студента та змініть
 його середній бал.
 """
+class Student:
+
+    def __init__(self, first_name, last_name, age, avarage_score = 3):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age 
+        self.avarage_score = avarage_score
+    
+    def change_score(self, avarage_score):
+        self.avarage_score = avarage_score + avarage_score * 0.1
+        return self.avarage_score
+
+if __name__ == "__name__":
+    student_1 = Student("Petro", "Onsienko", 19, 4.5)
+    #result = student_1.change_score()
+    print(student_1.first_name, student_1.last_name, student_1.age, student_1.avarage_score)
+
