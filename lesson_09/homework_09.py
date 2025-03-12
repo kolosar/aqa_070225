c96d8272b193887e16f3ca3917b54acf4acc7209
kolosar <verakolosar@gmail.com>
2025-03-12 21:08:57 +0100

    homework_09 is done

--- lesson_09/homework_09.py
+++ lesson_09/homework_09.py
@@ -6,7 +6,7 @@
 """
 class Student:
 
-    def __init__(self, first_name, last_name, age, avarage_score = 3):
+    def __init__(self, first_name, last_name, age, avarage_score):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age 
@@ -15,9 +15,14 @@ class Student:
     def change_score(self, avarage_score):
         self.avarage_score = avarage_score + avarage_score * 0.1
         return self.avarage_score
-
+""" 
 if __name__ == "__name__":
     student_1 = Student("Petro", "Onsienko", 19, 4.5)
     #result = student_1.change_score()
     print(student_1.first_name, student_1.last_name, student_1.age, student_1.avarage_score)
 
+"""
+student_1 = Student("Petro", "Onsienko", 19, 4.5)
+print(student_1.first_name, student_1.last_name, student_1.age, student_1.avarage_score)
+result = student_1.change_score(3)
+print(student_1.first_name, student_1.last_name, student_1.age, result)
